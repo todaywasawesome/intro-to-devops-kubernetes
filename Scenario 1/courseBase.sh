a0148bd355d7bc2afc2de4724ba83b63e3c3768d
Dan Garfield <dan@todaywasawesome.com>
2020-03-03 22:30:42 -0700

    install codefresh cli

--- Scenario 1/courseBase.sh
+++ Scenario 1/courseBase.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-touch ~/myfile.txt
+wget https://github.com/codefresh-io/cli/releases/download/v0.43.6/codefresh-v0.43.6-linux-x64.tar.gz
+tar -xzvf codefresh-v0.43.6-linux-x64.tar.gz
+chmod +x codefresh
+mv codefresh /bin/
